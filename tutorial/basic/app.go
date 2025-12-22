dd626c1f55a2846a7f76a51177c25d5d8337283f
ybuilds <yashas1145@proton.me>
2025-12-22 20:03:50 +0530

    refactoring investment function

--- tutorial/basic/app.go
+++ tutorial/basic/app.go
@@ -1,5 +1,5 @@
 package main
 
 func main() {
-	eulerPhi(10);
+	investment();
 }
--- tutorial/basic/invest.go
+++ tutorial/basic/invest.go
@@ -5,6 +5,14 @@ import (
 	"math"
 )
 
+const inflationRate float64 = 2.5;
+
+func calculateValues(investmentAmount float64, expectedReturnRate float64, years float64) (float64, float64){
+	var futureValue float64 = investmentAmount * math.Pow(1 + expectedReturnRate / 100, float64(years));
+	var futureRealValue float64 = futureValue / math.Pow(1 + inflationRate / 100.0, years);
+	return futureValue, futureRealValue;
+}
+
 func investment() {
 	var investmentAmount, expectedReturnRate, years float64;
 
@@ -16,11 +24,8 @@ func investment() {
 
 	fmt.Print("Enter time period in years: ");
 	fmt.Scan(&years);
-
-	const inflationRate float64 = 2.5;
 	
-	futureValue := investmentAmount * math.Pow(1 + expectedReturnRate / 100, float64(years));
-	futureRealValue := futureValue / math.Pow(1 + inflationRate / 100.0, years);
+	futureValue, futureRealValue := calculateValues(investmentAmount, expectedReturnRate, years);
 	
 	fmt.Println("Expected return value: ", math.Round(futureValue));
 	fmt.Println("Expected real return value: ", math.Round(futureRealValue));
