d462003dfcc100094094101fc2028e207f49821c
= <yashas1145@proton.me>
2026-01-09 08:23:58 +0530

    leetcode problems

---
+++ leet/arrays/moveZeroes.go
@@ -0,0 +1,30 @@
+package main
+
+import "fmt"
+
+func moveZeroes(nums []int) {
+	i, j := 0, 0
+
+	for i < len(nums) {
+		if nums[i] == 0 {
+			i++
+			continue
+		}
+
+		if i != j {
+			nums[i], nums[j] = nums[j], nums[i]
+		}
+		i++
+		j++
+	}
+}
+
+func moveZeroesDriver() {
+	nums := []int{0, 1, 0, 3, 12}
+	moveZeroes(nums)
+
+	for i := range nums {
+		fmt.Printf("%d ", nums[i])
+	}
+	fmt.Println()
+}
