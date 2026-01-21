19effeee39ba8552b87d7e7c5ec9768f385f4e17
= <yashas1145@proton.me>
2026-01-21 07:40:27 +0530

    refactor networking code

--- networks/stopWait.go
+++ networks/stopWait.go
@@ -1,33 +1,13 @@
 package main
 
-import (
-	"fmt"
-	"math/rand"
-	"time"
-)
+func swSender() {
 
-func lost() bool {
-	probabilty := 35
-	return rand.Int()%100 < probabilty
 }
 
-func stopWait() {
-	var frame int
+func swReceiver() {
 
-	fmt.Print("Enter frame size: ")
-	fmt.Scan(&frame)
+}
 
-	i := 0
-	for i < frame {
-		fmt.Printf("sending frame[%d]...\n", i)
+func swDriver() {
 
-		if lost() {
-			fmt.Printf("frame[%d] lost\n", i)
-			time.Sleep(2 * time.Second)
-		} else {
-			fmt.Printf("received frame[%d]\n", i)
-			i++
-			time.Sleep(1 * time.Second)
-		}
-	}
 }
